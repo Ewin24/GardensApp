74ebaf820b158da0a9d7daa6866e957d18064673
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2023-11-18 22:14:47 -0500

    feat: :sparkles: BaseController

--- Api/Controllers/BaseController.cs
+++ Api/Controllers/BaseController.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
-    public class BaseController
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController:ControllerBase
     {
-        
+
     }
 }
