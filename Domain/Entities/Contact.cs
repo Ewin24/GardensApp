3dd533dfad58a4de9dc4f9bda39886156a18ab94
SerranoValenciaJuanDavid <serranovalenciajuandavid949@gmail.com>
2023-11-19 11:31:04 -0500

    feat: :sparkles: scaffolding

---
+++ Domain/Entities/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class Contact
+{
+    public int Id { get; set; }
+
+    public string ContactName { get; set; } = null!;
+
+    public string ContactLastName { get; set; } = null!;
+
+    public string PostalCode { get; set; } = null!;
+
+    public virtual ICollection<Client> Clients { get; set; } = new List<Client>();
+}
---
+++ Domain/Entities/Proveedor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class Proveedor
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int? DentificationArd { get; set; }
+
+    public int? Cellphone { get; set; }
+
+    public int? IdCompaniesFk { get; set; }
+
+    public virtual Company? IdCompaniesFkNavigation { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
