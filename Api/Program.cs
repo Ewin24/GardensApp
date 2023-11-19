89aa0c24ed776e38f7dbb046ba55f7acac79f259
ewin24 <edwintrigos24@gmail.com>
2023-11-19 11:33:35 -0500

    feat: :bricks: Infrastructure changes

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,3 @@
-using System.Reflection;
-using Api.Extensions;
-using Microsoft.EntityFrameworkCore;
-using Persistence.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,15 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
-builder.Services.ConfigureCors();
-builder.Services.AddApplicationServices(); 
-
-builder.Services.AddDbContext<GardenApiContext>(option =>
-{
-    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
-    option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-});
 
 var app = builder.Build();
 
@@ -28,22 +14,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
-using (var scope = app.Services.CreateScope())
-{
-	var services = scope.ServiceProvider;
-	var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-	try
-	{
-		var context = services.GetRequiredService<GardenApiContext>();
-		await context.Database.MigrateAsync();
-	}
-	catch (Exception ex)
-	{
-		var _logger = loggerFactory.CreateLogger<Program>();
-		_logger.LogError(ex, "Ocurrio un error durante la migracion");
-	}
 }
 
 app.UseHttpsRedirection();
