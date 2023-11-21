d569c8bfbbe749fb51629d122950667217fd5ca6
ewin24 <edwintrigos24@gmail.com>
2023-11-21 12:45:28 -0500

    Final Commit

--- API/Dto/ProductDto.cs
+++ API/Dto/ProductDto.cs
@@ -7,22 +7,12 @@ namespace API.Dto
 {
     public class ProductDto
     {
-<<<<<<< HEAD
-        public string Id { get;  }
+        public string Id { get; set; }
 
         public string Name { get;  }
 
         public string ProductLine { get;  } 
 
         public string Dimensions { get;  }
-=======
-         public string Id { get;set;  }
-
-        public string Name { get;set;  }
-
-        public string ProductLine { get;set;  } 
-
-        public string Dimensions { get;set;  }
->>>>>>> b6c624fade8b0aa79ac0a930d6fa624f1fb0b9c8
     }
 }
